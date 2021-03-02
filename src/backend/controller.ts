80a3eed47d9bb65a2130d64d70a613fddd60b85b
Bahtiar <zam@kitalulus.com>
2021-03-02 22:17:41 +0700

    update: condition when get_device

--- src/backend/controller.ts
+++ src/backend/controller.ts
@@ -4,27 +4,34 @@ import Mqtt from './mqtt'
 const execute = (payload:any) => {
 
     const {intents, entities, traits} = payload;
+
     
     const intent = intents
         .filter((i:any) => i.confidence > 0.90)
-        .map((i:any) => i.name)    
-
-    const entity = entities["device:device"]
-        .filter((device:any) => (device.confidence > 0.90))
-        .map((d:any) =>  d.value)
+        .map((i:any) => i.name) 
         
-    if(intent[0] == "set_device"){
-        const trait = traits["wit$on_off"]
-            .filter((t:any) => t.confidence > 0.90)
-            .map((obj:any) => obj.value)
+    if(intent[0] != "get_device"){
 
-        const status = trait[0] == "on" ? "1" : "0"
-        Mqtt.pub(entity[0],status)
-    }else if(intent[0] == "get_device") {
-        Mqtt.pub("pompa-stats","1")
+        const entity = entities["device:device"]
+            .filter((device:any) => (device.confidence > 0.90))
+            .map((d:any) =>  d.value)
+            
+        if(intent[0] == "set_device"){
+            const trait = traits["wit$on_off"]
+                .filter((t:any) => t.confidence > 0.90)
+                .map((obj:any) => obj.value)
+
+            const status = trait[0] == "on" ? "1" : "0"
+            Mqtt.pub(entity[0],status)
+        }else{
+            console.log("not found")
+        }
     }else{
-        console.log("not found")
+        // when get_device 
+        Mqtt.pub("pompa-stats","1")
     }
+
+    
 }
 
 export default {
